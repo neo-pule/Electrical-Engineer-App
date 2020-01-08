0467b7608d2cfbf51e9b1493068018bdc9685e76
neo-pule <malemelaneopule@gmail.com>
2020-01-08 09:57:07 +0200

    collection calling

--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { SCCSkillsService } from '../../services/scc-skills.service';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.page.html',
@@ -9,11 +10,21 @@ export class MenuPage implements OnInit {
  subject : string;
  message : string;
  service : string;
-  constructor(private route : Router) { }
+
+ obj :any;
+
+  constructor(private route : Router,private list : SCCSkillsService) { }
 
   next(){
     this.route.navigateByUrl('info');
     // this.route.navigate(['info'], {queryParams : {subject : this.subject,service : this.service, message : this.message}});
+  }
+  getData(){
+    this.list.getInfo().subscribe(data => {
+      console.log(data);
+      this.obj = data;
+    })
+   console.log("new install")
   }
   ngOnInit() {
   }
